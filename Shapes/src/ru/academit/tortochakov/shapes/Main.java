51b9d41ce14dc1782d9c431eecb8fe077a8241cf
Саня <tortochakov27@gmail.com>
2017-01-25 22:48:59 +0700

    delete comma

--- Shapes/src/ru/academit/tortochakov/shapes/Main.java
+++ Shapes/src/ru/academit/tortochakov/shapes/Main.java
@@ -2,6 +2,6 @@ package ru.academit.tortochakov.shapes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello world!");
     }
 }
